8b28c20a01384928862594f1d9e5c85c755feae6
b-kostenko <bogdankostenko83@gmail.com>
2025-10-29 16:59:22 +0200

    feat: update API base URL to production server

--- src/api/BaseApi.ts
+++ src/api/BaseApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: "https://mxf2-backend.onrender.com/api/v1",
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
